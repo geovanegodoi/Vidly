77216275ce7f9291c5b61f1f182d670ef0037eff
GGodoi <geovane.godoi@gmail.com>
2018-02-22 21:12:30 -0300

    New feature : - Implemented the MoviesController through the Web API interface

---
+++ Vidly.Web/Controllers/Api/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Vidly.Core.BO;
+using Vidly.Interfaces.BO;
+using Vidly.TO;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : BaseApiController<long, MovieTO, MovieCriteriaTO, IMovieBO>
+    {
+        public MoviesController()
+        {
+            this.DefaultBO = new MovieBO();
+        }
+    }
+}
